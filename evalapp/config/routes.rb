de5e6832fd8c8df1741e3e5c96e0631775846e33
joshvarg <joshva528@gmail.com>
2022-04-17 02:00:29 -0400

    Added admin specific screens and feedback display

--- evalapp/config/routes.rb
+++ evalapp/config/routes.rb
@@ -4,12 +4,11 @@ Rails.application.routes.draw do
   get "/events", to: "events#index"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  get "/logout", to: "sessions#destroy"
   get "/signup", to: "students#new"
   post "/signup", to: "students#create"
   get "/feedback", to: "feedbacks#new"
   post "/feedback", to: "feedbacks#create"
-  
 
   resources :events
   resources :feedbacks
